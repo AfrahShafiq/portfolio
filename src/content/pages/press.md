81dc4583a72afa4036b64766946d4c91613966e7
sharmad247 <6037141+sharmad247@users.noreply.github.com>
2020-06-11 11:31:50 +0530

    press page template

---
+++ src/content/pages/press.md
@@ -0,0 +1,18 @@
+---
+template: press-page
+slug: /press
+title: About Me
+---
+# 2020
+
+[Article 1 - Source](www.google.com)
+
+[Article 2 - Source](www.google.com)
+
+# 2019
+
+Article 1 - Source
+
+Article 2 - Source
+
+Article 3 - Source
---
+++ src/templates/press-page.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export const pageQuery = graphql`
+  query PressQuery($id: String!){
+		markdownRemark(id: { eq: $id }) {
+      id
+			html
+			excerpt(pruneLength: 140)
+      frontmatter {
+        title
+      }
+    }
+  }
+`
+const AboutPage = ({ data }) => {
+	const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter, html, excerpt } = markdownRemark
+
+	return (
+		<Layout className="page" banner='homeBanner'>
+			<SEO
+				title={frontmatter.title}
+				description={excerpt}
+			/>
+			<div className="wrapper">
+				<article className='press' dangerouslySetInnerHTML={{ __html: html }} />
+			</div>
+		</Layout>
+	)
+}
+
+export default AboutPage
